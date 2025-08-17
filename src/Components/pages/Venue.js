de2245b94f02df84c778640bb17241432920ad8d
Ruhul Amin <ruhulamin.hit@gmail.com>
2025-08-17 13:52:53 +0530

    updated

--- src/Components/pages/Venue.js
+++ src/Components/pages/Venue.js
@@ -49,7 +49,7 @@ const Venue = () => {
       <div className="scrolling-strip">
         <ul className="scrolling-list">
         <li><a href="/Schedule">📅 Conference Date: 14-16 January, 2026</a></li>
-        <li><a href="/Schedule">📅 Deadline:14th August, 2025</a></li>
+        <li><a href="/Schedule">📅 Deadline:31st August, 2025</a></li>
          <li><a href="https://link.springer.com/book/9789819642724">Proceeding of ICISS 2024</a></li>
         <li><a href="/Schedule">📅 Conference Schedule</a></li>
           <li><a href="/news">📰 Latest News</a></li>
