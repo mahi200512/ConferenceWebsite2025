4a7eca2651dc35d9a189a6b538f4d8a8f02a8e1d
Ruhul Amin <ruhulamin.hit@gmail.com>
2025-09-03 12:01:04 +0530

    ok

--- src/Components/pages/Papers.js
+++ src/Components/pages/Papers.js
@@ -65,7 +65,7 @@ const Papers = () => {
       <div className="call-for-papers">
 
         <h2> <a style={{ color: '#0021f3', fontWeight: 'bold' }}>
-        Call for Papers (Paper submission Deadline:25th September 2025)
+        Call for Papers (Hard Deadline:10th October 2025)
         </a> </h2>
 
 
