4cc32d6aa3bd6a7c4ff44769938daa4a7cb512a2
Ruhul Amin <ruhulamin.hit@gmail.com>
2025-10-22 00:36:57 +0530

    any remarks

--- src/Components/pages/Schedule.js
+++ src/Components/pages/Schedule.js
@@ -23,7 +23,7 @@ const round1Dates = [
   { event: "Acceptance Notification", date: "1st October 2025" },
   { event: "Revised Notification (Due large No. of Paper submission)", date: "10th October 2025" },
   { event: "Camera Ready Paper Submission", date: "25th October 2025" },
-  { event: "Registration Deadline", date: "5th November 2025" },
+  { event: "Registration Deadline", date: "22nd October 2025" },
   { event: "Conference Date", date: "14-16 January, 2026" }
 ];
 
